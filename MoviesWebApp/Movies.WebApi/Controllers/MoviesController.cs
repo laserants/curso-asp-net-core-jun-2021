8fe66714bb63675d83dac5b07af847e4022f594a
Jose Ordoñez <josemaord@gmail.com>
2021-07-08 08:16:11 -0600

    Agregar documentacion XML y exportarlo Swagger

--- MoviesWebApp/Movies.WebApi/Controllers/MoviesController.cs
+++ MoviesWebApp/Movies.WebApi/Controllers/MoviesController.cs
@@ -12,6 +12,8 @@ namespace Movies.WebApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Produces("application/json")]
+    [Consumes("application/json")]
     public class MoviesController : ControllerBase
     {
         private readonly IMovieRepository _movieRepository;
@@ -21,6 +23,15 @@ namespace Movies.WebApi.Controllers
             _movieRepository = movieRepository;
         }
 
+        /// <summary>
+        /// Get all movies, and get movies by title.
+        /// </summary>
+        /// <remarks>
+        /// If you didn't send parameters the endpoint will return all movies.
+        /// </remarks>
+        /// <param name="title">Movie Title</param>
+        /// <returns></returns>
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<Movie>))]
         [HttpGet]
         public async Task<IActionResult> GetMoviesAsync(string title)
         {
@@ -36,6 +47,16 @@ namespace Movies.WebApi.Controllers
             return Ok(movies);
         }
 
+        /// <summary>
+        /// Get a movie by Id
+        /// </summary>
+        /// <remarks>
+        /// Send the Id in URI
+        /// </remarks>
+        /// <param name="id">Movie Id</param>
+        /// <returns></returns>
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Movie))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(string))]
         [HttpGet]
         [Route("{id}")]
         public async Task<IActionResult> GetMoviesAsync(int id)
@@ -48,6 +69,16 @@ namespace Movies.WebApi.Controllers
             return Ok(movie);
         }
 
+        /// <summary>
+        /// Create a movie
+        /// </summary>
+        /// <remarks>
+        /// Send the Movie data in HTTP Request's body as JSON
+        /// </remarks>
+        /// <param name="movie"></param>
+        /// <returns></returns>
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Movie))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(string))]
         [HttpPost]
         public async Task<IActionResult> CreateMovieAsync(Movie movie)
         {
@@ -59,6 +90,19 @@ namespace Movies.WebApi.Controllers
             return Ok(result);
         }
 
+        /// <summary>
+        /// Update a movie
+        /// </summary>
+        /// <remarks>
+        /// Send the Movie data in HTTP Request's body as JSON.
+        /// URI Id and Movie Id within JSON sould match.
+        /// </remarks>
+        /// <param name="id">Movie Id</param>
+        /// <param name="movie"></param>
+        /// <returns></returns>
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Movie))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(string))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(string))]
         [HttpPut]
         [Route("{id}")]
         public async Task<IActionResult> UpdateMovieAsync(int id, Movie movie)
@@ -70,11 +114,21 @@ namespace Movies.WebApi.Controllers
             var result = await _movieRepository.UpdateMovieAsync(movie);
             if (result == null)
             {
-                return NotFound("");
+                return NotFound("Movie Not Found");
             }
             return Ok(result);
         }
 
+        /// <summary>
+        /// Delete a movie
+        /// </summary>
+        /// <remarks>
+        /// Send Id in the URI
+        /// </remarks>
+        /// <param name="id">Movie Id</param>
+        /// <returns></returns>
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(string))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(string))]
         [HttpDelete]
         [Route("{id}")]
         public async Task<IActionResult> DeleteMovieAsync(int id)
